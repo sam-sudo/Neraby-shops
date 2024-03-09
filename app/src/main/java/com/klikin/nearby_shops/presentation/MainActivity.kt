6e1ae811bbdd97841a0d34fe1365a55a2a69b84e
sam-sudo <samuelhueso19@gmail.com>
2024-03-09 13:34:13 +0100

    Project created with the basics.

---
+++ app/src/main/java/com/klikin/nearby_shops/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.klikin.nearby_shops.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.klikin.nearby_shops.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
