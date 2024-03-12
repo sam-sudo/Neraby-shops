826b385d69f06d9c2d76e95d95a1037bb4f03e6f
sam-sudo <samuelhueso19@gmail.com>
2024-03-12 19:21:31 +0100

    Ask for permissions and handler flow done!

--- app/src/main/java/com/klikin/nearby_shops/presentation/MainActivity.kt
+++ app/src/main/java/com/klikin/nearby_shops/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.klikin.nearby_shops.presentation
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.klikin.nearby_shops.databinding.ActivityMainBinding
 import com.klikin.nearby_shops.presentation.usescases.shopsList.ShopListActivity
@@ -13,6 +16,23 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val PERMISSION_REQUEST_CODE = 1001
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                CoroutineScope(Dispatchers.Main).launch {
+                    delay(2000)
+                    openShopList()
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
@@ -22,10 +42,33 @@ class MainActivity : AppCompatActivity() {
 
         screenSplash.setKeepOnScreenCondition { true }
 
-        CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
-            openShopList()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // No tenemos permisos, así que los solicitamos
+            askForPermission()
+        } else {
+            CoroutineScope(Dispatchers.Main).launch {
+                delay(2000)
+                openShopList()
+            }
         }
+    }
+
+    fun askForPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ),
+            PERMISSION_REQUEST_CODE,
+        )
     }
 
     private fun openShopList() {
--- app/src/main/java/com/klikin/nearby_shops/presentation/usescases/shopsList/ShopListActivity.kt
+++ app/src/main/java/com/klikin/nearby_shops/presentation/usescases/shopsList/ShopListActivity.kt
@@ -1,8 +1,12 @@
 package com.klikin.nearby_shops.presentation.usescases.shopsList
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.klikin.nearby_shops.R
@@ -22,6 +26,8 @@ class ShopListActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        val llShopListScreen = binding.llShopListScreen
+        val llSnoPermissionsScreen = binding.llNoPermissionsGranted
         val card1 = binding.cardShops
         val card1Title = binding.tvShopLength
         val card1Text = binding.tvShopText
@@ -32,44 +38,58 @@ class ShopListActivity : AppCompatActivity() {
         val categoriesRecyclerView = binding.recyclerViewCategories
         var adapter = CategoryAdapter(mutableMapOf())
 
-        viewModel.loadShops()
-        viewModel.loadCategories()
-        adapter.setOnItemClickListener(
-            object : CategoryAdapter.onItemClickListener {
-                override fun onItemClick(position: Int) {
-                    val selectedCategory = viewModel.sate.value.categoriesMap.keys.toList()[position]
-                    Log.d("TAG", "ITEM TOUCHED -> $selectedCategory")
-                    viewModel.loadShopsByCategory(selectedCategory)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            llShopListScreen.visibility = View.GONE
+            llSnoPermissionsScreen.visibility = View.VISIBLE
+        } else {
+            llShopListScreen.visibility = View.VISIBLE
+            llSnoPermissionsScreen.visibility = View.GONE
+            viewModel.loadShops()
+            viewModel.loadCategories()
+            adapter.setOnItemClickListener(
+                object : CategoryAdapter.onItemClickListener {
+                    override fun onItemClick(position: Int) {
+                        val selectedCategory = viewModel.sate.value.categoriesMap.keys.toList()[position]
+                        Log.d("TAG", "ITEM TOUCHED -> $selectedCategory")
+                        viewModel.loadShopsByCategory(selectedCategory)
+                    }
+                },
+            )
+            adapter.updateData(viewModel.sate.value.categoriesMap)
+            categoriesRecyclerView.adapter = adapter
+            lifecycleScope.launch {
+                viewModel.sate.collect { state ->
+                    card1Title.text = state.shopList.size.toString()
+                    binding.recyclerViewShops.adapter = ShopAdapter(state)
                 }
-            },
-        )
-        adapter.updateData(viewModel.sate.value.categoriesMap)
-        categoriesRecyclerView.adapter = adapter
-        lifecycleScope.launch {
-            viewModel.sate.collect { state ->
-                card1Title.text = state.shopList.size.toString()
-                binding.recyclerViewShops.adapter = ShopAdapter(state)
             }
-        }
 
-        // Cards
-        card1.setOnClickListener {
-            card1.background.setTint(ContextCompat.getColor(this, R.color.colorCardOnTap))
-            card1Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
-            card1Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
+            // Cards
+            card1.setOnClickListener {
+                card1.background.setTint(ContextCompat.getColor(this, R.color.colorCardOnTap))
+                card1Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
+                card1Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
 
-            card2.background.setTint(ContextCompat.getColor(this, R.color.colorCardNoTap))
-            card2Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTitleNoTap))
-            card2Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextNoTap))
-        }
-        card2.setOnClickListener {
-            card1.background.setTint(ContextCompat.getColor(this, R.color.colorCardNoTap))
-            card1Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTitleNoTap))
-            card1Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextNoTap))
+                card2.background.setTint(ContextCompat.getColor(this, R.color.colorCardNoTap))
+                card2Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTitleNoTap))
+                card2Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextNoTap))
+            }
+            card2.setOnClickListener {
+                card1.background.setTint(ContextCompat.getColor(this, R.color.colorCardNoTap))
+                card1Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTitleNoTap))
+                card1Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextNoTap))
 
-            card2.background.setTint(ContextCompat.getColor(this, R.color.colorCardOnTap))
-            card2Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
-            card2Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
+                card2.background.setTint(ContextCompat.getColor(this, R.color.colorCardOnTap))
+                card2Title.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
+                card2Text.setTextColor(ContextCompat.getColor(this, R.color.colorCardTextOnTap))
+            }
         }
     }
 }
